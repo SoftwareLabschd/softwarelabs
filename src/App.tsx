186f036d5e2934212ed65d13c36050f8d18654c7
Paras367 <dhimanparas605@gmail.com>
2026-02-16 22:39:55 +0530

    Add ErrorBoundary back

--- src/App.tsx
+++ src/App.tsx
@@ -22,4 +22,18 @@ function App() {
   )
 }
 
+
+import ErrorBoundary from "@/components/ErrorBoundary";
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <div style={{ padding: '20px', color: 'white' }}>
+        <h1>🧗 Test Page</h1>
+        <p>If you see this, App.tsx is working!</p>
+      </div>
+    </ErrorBoundary>
+  )
+}
+
 export default App
